1435f3cc7a29fc2aad657219b763d94da2fe0ee1
Nikunj Gupta <nikunj.gupta@hashedin.com>
2020-03-26 19:34:56 +0530

    deal cards

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
 
 // const socket = socketIOClient("http://18.216.249.155:4001");
-const socket = socketIOClient("http://localhost:4001");
+const socket = socketIOClient("http://192.168.0.115:4001");
 export default socket;
