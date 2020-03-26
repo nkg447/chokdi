0df02b8f87ecdf1da50a9912d6f9edbddda19222
Nikunj Gupta <nikunj.gupta@hashedin.com>
2020-03-26 17:23:58 +0530

    trump decide

---
+++ src/component/trumpdecide/trumpdecide.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import socket from "../../socket";
+const suits = ["S", "H", "C", "D"];
+export default props => {
+  const { username } = props;
+  const [trump, setTrump] = useState("S");
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ alignSelf: "center" }}>Decide Trump</div>
+      <div className="trump-option">
+        {suits.map(s => (
+          <div
+            style={trump == s ? { backgroundColor: "blue" } : {}}
+            onClick={() => setTrump(s)}
+          >
+            {s}
+          </div>
+        ))}
+      </div>
+      <div
+        style={{ alignSelf: "center" }}
+        onClick={() => {
+          socket.emit("trump-decide", { username, trump });
+        }}
+        className="button"
+      >
+        SUBMIT
+      </div>
+    </div>
+  );
+};
--- src/container/game/game.js
+++ src/container/game/game.js
@@ -2,28 +2,43 @@ import React, { Component } from "react";
 import MyHand from "../myhand/myhand";
 import OpponentHand from "../../component/oponenthand/opponenthand";
 import TellHands from "../../component/tellhands/tellhands";
+import TrumpDecide from "../../component/trumpdecide/trumpdecide";
+
 export default class Game extends Component {
   render() {
     const gameRoomData = JSON.parse(this.props.gameRoomData);
     const username = this.props.username;
     console.log(gameRoomData, username);
-    const { players, gameCode, playersOrder, status } = gameRoomData;
+    const { players, gameCode, playersOrder, status, trump } = gameRoomData;
 
     if (status === "WAITING_TO_JOIN") {
       return <p>Waiting for other players, JOIN GAME {gameCode}</p>;
     }
-
+    let boardComponent = undefined;
     if (status === "TELL_HANDS") {
       if (username === gameRoomData.tellHandsUsername) {
-        return (
+        boardComponent = (
           <TellHands
             username={username}
             minHands={gameRoomData.minHandsToTell}
           ></TellHands>
         );
       } else {
-        return (
+        boardComponent = (
           <p>Waiting for {gameRoomData.tellHandsUsername} to tell Hands</p>
+        );
+      }
+    }
+    if (status === "TRUMP_DECIDE") {
+      if (username === gameRoomData.trumpTeller) {
+        boardComponent = <TrumpDecide username={username}></TrumpDecide>;
+      } else {
+        boardComponent = (
+          <p>
+            Waiting for {gameRoomData.trumpTeller} to tell Trump.{" "}
+            {gameRoomData.trumpTeller} will make {gameRoomData.minHandsToTell}{" "}
+            hands
+          </p>
         );
       }
     }
@@ -48,8 +63,9 @@ export default class Game extends Component {
           })}
         <div className="me">
           <MyHand hand={players[username].cards}></MyHand>
+          <h3>Trump - {trump}</h3>
         </div>
-        <div className="board"></div>
+        <div className="board">{boardComponent}</div>
       </div>
     );
   }
